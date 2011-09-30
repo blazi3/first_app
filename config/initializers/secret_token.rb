c82dfdadf6015cc109f1c709bfbe526a15d37be1
Blaz <blaz@westnet.com.au>
2011-09-30 23:07:43 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a7e0ba01325fda02d0a92f446bb16d298fa3338674d77f495c20c08e4e818758677e33e637855b54acd1f9433789725a554903ee37efc3d635e6fdfbba95a93c'
